35c025384ffb69ce0027607dd23fdb628ffef2aa
Subham Jana <subhamjana2013@gmail.com>
2026-01-05 23:11:20 +0530

    Initial commit – OrderFlow Phase 1 backend

---
+++ src/main/java/com/orderflow/OrderflowOmsApplication.java
@@ -0,0 +1,13 @@
+package com.orderflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowOmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderflowOmsApplication.class, args);
+	}
+
+}
