6e9b4f7949aed741da679a9a9b551b4489f46d93
Subham Jana <subhamjana2013@gmail.com>
2026-01-06 11:30:09 +0530

    Add OrderController with place and get APIs

---
+++ src/main/java/com/orderflow/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.orderflow.controller;
+
+import com.orderflow.domain.Order;
+import com.orderflow.dto.OrderRequest;
+import com.orderflow.dto.OrderResponse;
+import com.orderflow.service.OrderService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+    @PostMapping
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest request){
+        return ResponseEntity.ok(orderService.placeOrder(request));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrder(@PathVariable Long id){
+        return ResponseEntity.ok(orderService.getOrderById(id));
+    }
+}
